56da41e9327d0f8fae8f1409aea1ec306c4d0d17
dramforever <dramforever@live.com>
2018-07-08 17:48:06 +0800

    Custom ruleset

--- config.py
+++ config.py
@@ -6,9 +6,11 @@
 
 Cer by @xmcp.
 https://github.com/xmcp/cer
+
+Rule by dramforever
 """
 
-description='“X可以随便接”' #简要介绍这个规则
+description='“第二个字母接倒数第二个”' #简要介绍这个规则
 life_step=2 #超时减血的时间（秒），应为正整数
 max_live=10 #玩家的生命值上限，应为正整数
 skip_cost=3 #跳过当前回合所需的生命值，应为非负整数
@@ -17,7 +19,7 @@ import random
 
 history=set()
 with open('dict.txt','r') as f:
-    words=set(f.read().split('\n'))
+    words=set((s for s in f.read().split('\n') if len(s) > 1))
 
 def init():
     """ 当每局新游戏开始时被调用。
@@ -34,16 +36,21 @@ def validate(before,after):
     参数 before 是当前的单词，after 是玩家尝试提交的单词。
     如果这次提交合法，返回 None；否则返回一个字符串，解释为什么这样不合法。
     """
-    after=after.strip()
+    after=after.strip().lower()
+
     if after in history:
         return '单词已经用过了'
     elif not after.isalpha():
         return '只能包含字母'
-    elif before[-1]!=after[0]:
-        return '首字母不符合要求'
-    elif before[-1]!='x' and after not in words:
+    elif len(after) < 1:
+        return '至少应有两个字母'
+    elif before[-2] != after[1]:
+        return '第二个字母应为 {}'.format(before[-2])
+    elif after not in words:
         return '单词不在词典中'
-    history.add(after)
+    else:
+        history.add(after)
+        return None
 
 def skip(before):
     """ 当玩家试图跳过当前回合时被调用。
